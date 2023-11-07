c77e153b4ae0967a16d0b9116c1b2e567b32fcd5
sw586 <sw586@126.com>
2023-11-07 23:52:57 +0800

    Update x_port_R.sh

--- x_port_R.sh
+++ x_port_R.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-# 第一步：下载port.txt并更新config.json
-PORT=$(curl https://raw.githubusercontent.com/sw586/MYx/master/port.txt)
-sed -i "22s/\"port\": [0-9]*,/\"port\": $PORT,/" /root/Xray/config.json
+# 检查jq是否已安装
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found, installing now."
+    # 根据您的系统选择合适的安装命令
+    # 以下是在基于Debian的系统上安装jq的命令
+    sudo apt-get update && sudo apt-get install -y jq
+fi
 
+# 现在jq应该已经安装了，接下来执行更新config.json的命令
+PORT=$(curl -s https://raw.githubusercontent.com/sw586/MYx/master/port.txt)
+jq '.inbounds[0].port = $newport' --argjson newport $PORT /root/Xray/config.json > /root/Xray/config.json.tmp && mv /root/Xray/config.json.tmp /root/Xray/config.json
+
+# 检查命令是否成功
+if [ $? -eq 0 ]; then
+    echo "Port updated successfully."
+else
+    echo "Failed to update the port."
+fi
