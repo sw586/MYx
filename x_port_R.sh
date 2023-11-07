c3942b5465dbe341111daf8b8ba3e6ef6aabd643
sw586 <sw586@126.com>
2023-11-08 00:01:29 +0800

    Update x_port_R.sh

--- x_port_R.sh
+++ x_port_R.sh
@@ -2,7 +2,7 @@
 
 # 检查jq是否已安装
 if ! command -v jq &> /dev/null; then
-    echo "jq could not be found, installing now."
+    echo "未发现jq，现在开始安装..."
     # 根据您的系统选择合适的安装命令
     # 以下是在基于Debian的系统上安装jq的命令
     sudo apt-get update && sudo apt-get install -y jq
@@ -10,20 +10,27 @@ fi
 
 # 现在jq应该已经安装了，接下来执行更新config.json的命令
 PORT=$(curl -s https://raw.githubusercontent.com/sw586/MYx/master/port.txt)
-jq '.inbounds[0].port = $newport' --argjson newport $PORT /root/Xray/config.json > /root/Xray/config.json.tmp && mv /root/Xray/config.json.tmp /root/Xray/config.json
+if jq '.inbounds[0].port = $newport' --argjson newport $PORT /root/Xray/config.json > /root/Xray/config.json.tmp; then
+    if [ -s /root/Xray/config.json.tmp ]; then
+        mv /root/Xray/config.json.tmp /root/Xray/config.json
+        echo "端口更新成功。"
 
-# 检查命令是否成功
-if [ $? -eq 0 ]; then
-    echo "Port updated successfully."
+        # 结束xray进程
+        pkill -f xray
+
+        # 等待一秒钟，确保xray进程已经完全停止
+        sleep 1
+
+        # 重新启动xray服务
+        /root/Xray/xray -config=/root/Xray/config.json &
+
+        # 输出xray进程信息
+        echo "xray进程信息："
+        pgrep -af xray
+
+    else
+        echo "更新端口失败：临时文件为空。"
+    fi
 else
-    echo "Failed to update the port."
+    echo "更新端口失败：jq命令执行出错。"
 fi
-
-# 第二步：结束xray进程
-pkill -f xray
-
-# 第四步：创建名为xray的screen会话并执行命令
-screen -dmS xray bash -c 'iptables -F && iptables -X && iptables -F -t nat && iptables -X -t nat && iptables -P FORWARD ACCEPT && iptables -P INPUT ACCEPT && iptables -P OUTPUT ACCEPT && /root/Xray/xray -config=/root/Xray/config.json'
-
-# 检查screen会话是否创建成功
-screen -list | grep xray
